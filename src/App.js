2c005f6847730e766640c50e7fb8bc3033c3df34
RishikeshSV <rishuvidyarthi@gmail.com>
2023-09-29 19:01:46 +0530

    added key event

--- src/App.js
+++ src/App.js
@@ -1,8 +1,57 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "./App.css";
 
 function App() {
-  return <React.Fragment>WORDLE</React.Fragment>;
+  const [wordle, setWordle] = useState(["s", "t", "u", "n"]);
+  const [wordleAttempt, setWordleAttempt] = useState([]);
+  const [attempts, setAttempts] = useState(6);
+
+  const latestWordleAttempt = useRef(wordleAttempt);
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, []);
+
+  const handleKeyDown = (e) => {
+    if (
+      /^[a-z]$/.test(e.key) &&
+      latestWordleAttempt.current.length < wordle.length
+    )
+      setWordleAttempt((prev) => [...prev, e.key]);
+  };
+
+  useEffect(() => {
+    // Update the ref whenever wordleAttempt changes
+    latestWordleAttempt.current = wordleAttempt;
+  }, [wordleAttempt]);
+
+  const checkAttempt = () => console.log("TRY NEXT ATTEMPT");
+
+  return (
+    <div className="word-container">
+      {[...Array(attempts)].map((attempt, index) => (
+        <div className="word" key={`Attempt - ${index}`}>
+          {wordle.map((letter, i) => (
+            <div
+              key={`Letter - ${i}`}
+              // className={wordleAttempt[i] ? "letter" : "blank"}
+            >
+              {wordleAttempt[i] ? (
+                <div className="letter">{wordleAttempt[i].toUpperCase()}</div>
+              ) : (
+                <div className="blank">&nbsp;</div>
+              )}
+            </div>
+          ))}
+        </div>
+      ))}
+      <button className="submit" onClick={() => checkAttempt()}>
+        SUBMIT
+      </button>
+      <button className="delete">DELETE</button>
+    </div>
+  );
 }
 
 export default App;
