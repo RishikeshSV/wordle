acfda21079f6b6b792f86162246b1a57f75a64c7
RishikeshSV <rishuvidyarthi@gmail.com>
2023-10-04 20:16:17 +0530

    added validations for keyboard

--- src/KeyboardMobile.jsx
+++ src/KeyboardMobile.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Keyboard from "react-simple-keyboard";
 import "react-simple-keyboard/build/css/index.css";
@@ -11,9 +11,83 @@ const keyboardLayout = {
   ],
 };
 
-const KeyboardMobile = ({ handleKeyDown }) => {
+const KeyboardMobile = ({ handleKeyDown, keyColors = [] }) => {
+  const [keys, setKeys] = useState({ exists: [], incorrect: [], correct: [] });
+  const [buttonTheme, setButtonTheme] = useState([]);
+
+  useEffect(() => {
+    if (keyColors.style?.length) {
+      const newKeys = updateKeys(keyColors, keys);
+      setKeys(newKeys);
+    }
+  }, [keyColors]);
+
+  useEffect(() => {
+    const tempButtonTheme = [];
+    if (keys.exists.length) {
+      tempButtonTheme.push({
+        class: "hg-exists",
+        buttons: keys.exists.join(" "),
+      });
+    }
+
+    if (keys.incorrect.length) {
+      tempButtonTheme.push({
+        class: "hg-incorrect",
+        buttons: keys.incorrect.join(" "),
+      });
+    }
+
+    if (keys.correct.length) {
+      tempButtonTheme.push({
+        class: "hg-correct",
+        buttons: keys.correct.join(" "),
+      });
+    }
+
+    setButtonTheme(tempButtonTheme);
+  }, [keys]);
+
+  const updateKeys = (keyColors, keys) => {
+    const { style, word } = keyColors;
+    const temp = { correct: [], incorrect: [], exists: [] };
+
+    word.forEach((letter, i) => {
+      temp[style[i]].push(letter);
+    });
+
+    const newKeys = {
+      exists: removeDuplicates([...keys.exists, ...temp.exists]),
+      incorrect: removeDuplicates([...keys.incorrect, ...temp.incorrect]),
+      correct: removeDuplicates([...keys.correct, ...temp.correct]),
+    };
+
+    //correct > incorrect > exists is the priority
+    //if letter is in correct, then remove from incorrect and exists
+    newKeys.correct.forEach((letter) => {
+      newKeys.incorrect = removeItems(newKeys.incorrect, [letter]);
+      newKeys.exists = removeItems(newKeys.exists, [letter]);
+    });
+
+    //if letter is in incorrect, then remove from exists
+    newKeys.incorrect.forEach((letter) => {
+      newKeys.exists = removeItems(newKeys.exists, [letter]);
+    });
+
+    return newKeys;
+  };
+
+  const removeDuplicates = (array) => [...new Set(array)];
+
+  const removeItems = (sourceArray, itemsToRemove) =>
+    sourceArray.filter((item) => !itemsToRemove.includes(item));
+
   return (
-    <Keyboard onKeyPress={(e) => handleKeyDown(e)} layout={keyboardLayout} />
+    <Keyboard
+      onKeyPress={(e) => handleKeyDown(e)}
+      layout={keyboardLayout}
+      buttonTheme={buttonTheme}
+    />
   );
 };
 
